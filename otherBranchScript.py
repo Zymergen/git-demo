ead17da4cdcf20a57b3f7acddafcf72b9ab691f6
Aaron Kimball <aaron@zymergen.com>
2016-05-02 19:29:23 -0700

    Add a helpful number printing loop.

--- otherBranchScript.py
+++ otherBranchScript.py
@@ -8,5 +8,11 @@ def main():
     print "Here is a script on the other branch."
     print "Like all the scripts in this repo, it has disappointingly little to do."
 
+    print "Here are some numbers for you to look at."
+    x = 0
+    while x < 24:
+        print x
+        x = x + 1
+
 if __name__ == "__main__": 
     main()
