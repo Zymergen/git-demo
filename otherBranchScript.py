228a6dd8f3eba481dbc25e799982e141fbac4305
Aaron Kimball <aaron@zymergen.com>
2016-05-02 19:28:38 -0700

    Add a script on the other branch.

---
+++ otherBranchScript.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+#
+# (c) Copyright 2016 Zymergen, Inc.
+# All Rights Reserved.
+
+
+def main():
+    print "Here is a script on the other branch."
+    print "Like all the scripts in this repo, it has disappointingly little to do."
+
+if __name__ == "__main__": 
+    main()
